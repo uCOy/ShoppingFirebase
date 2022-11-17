bf240bdf3135744051cca7bd6683ec48ec3d8fc2
RICHARD HENRIQUE COUTO DA ROCHA <richard.hcrocha@senacsp.edu.br>
2022-11-16 22:21:52 -0300

    Teste

---
+++ src/components/Product/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import firestore from '@react-native-firebase/firestore'
+import { ButtonIcon } from '../ButtonIcon';
+import { Container, Info, Title, Quantity, Options } from './styles';
+
+export type ProductProps = {
+  id: string;
+  description: string;
+  quantity: number;
+  done: boolean;
+}
+
+type Props = {
+  data: ProductProps;
+}
+
+export function Product({ data }: Props) {
+  function handleDoneToggle(){
+    firestore()
+    .collection('products')
+    .doc(data.id)
+    .update({
+      done:!data.done
+    })
+  }
+
+  function handleDelete(){
+    firestore()
+    .collection('products')
+    .doc(data.id)
+    .delete()
+  }
+
+  return (
+    <Container>
+      <Info>
+        <Title done={data.done}>
+          {data.description}
+        </Title>
+
+        <Quantity>
+          Quantidade: {data.quantity}
+        </Quantity>
+      </Info>
+
+      <Options>
+        <ButtonIcon
+          onPress={handleDoneToggle}
+          icon={data.done ? "undo" : "check"}
+        />
+
+        <ButtonIcon
+          icon="delete"
+          color="alert"
+          onPress={handleDelete}
+        />
+      </Options>
+    </Container>
+  );
+}
